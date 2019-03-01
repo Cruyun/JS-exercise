4b5addf90e15bab974d3519a9e494d287be03aa1
Cruyun <1045671796@qq.com>
2019-03-01 22:36:33 +0800

    curry adder

---
+++ curry_adder.js
@@ -0,0 +1,25 @@
+// 科里化求和函数
+
+var adder = function () {
+    var _args = [];
+    return function () {
+        if (arguments.length === 0) {
+            return _args.reduce(function (a, b) {
+                return a + b;
+            });
+        }
+        [].push.apply(_args, [].slice.call(arguments));
+        return arguments.callee; // *callee*是arguments对象的一个属性。它可以用于引用该函数的函数体内当前正在执行的函数
+    }
+};    
+var sum = adder();
+
+console.log(sum);     // Function
+
+sum(100,200)(300);    // 调用形式灵活，一次调用可输入一个或者多个参数，并且支持链式调用
+sum(400);
+console.log(sum());   // 
+
+
+// 上面 adder是柯里化了的函数，它返回一个新的函数，新的函数接收可分批次接受新的参数，延迟到最后一次计算。
+
